2830483330cbfdeca01f11b6f219d393f591e1bf
Cameron Clough <cameronjclough@gmail.com>
2023-08-17 15:59:23 +0100

    test app loads (#10)

---
+++ src/app/App.test.jsx
@@ -0,0 +1,10 @@
+import { Suspense } from 'react'
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+
+import Page from './page';
+
+test('renders without crashing', () => {
+  render(<Suspense fallback="loading"><Page /></Suspense>)
+  expect(screen.getByText('flash.comma.ai')).toBeInTheDocument()
+})
