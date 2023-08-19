5fbb068bf9f5f891394aeea10f16bb54d9747cb8
Cameron Clough <cameronjclough@gmail.com>
2023-08-19 01:02:11 +0100

    rm

--- src/config.js
+++ src/config.js
@@ -2,7 +2,6 @@ const config = {
   manifests: {
     release3: 'https://raw.githubusercontent.com/commaai/openpilot/release3/system/hardware/tici/agnos.json',
     master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/agnos.json',
-    staging: 'https://raw.githubusercontent.com/commaai/openpilot/agnos-alt/system/hardware/tici/agnos.json',
   },
 }
 
