a4befb3f704e22241ee8107d09f22b4960729100
Cameron Clough <cameronjclough@gmail.com>
2023-08-20 00:46:59 +0100

    test

--- src/utils/manifest.test.js
+++ src/utils/manifest.test.js
@@ -1,5 +1,8 @@
 import { expect, test } from 'vitest'
 
+import jsSHA from 'jssha'
+import pako from 'pako'
+
 import config from '../config'
 import { getManifest } from './manifest'
 
@@ -11,19 +14,45 @@ for (const [branch, manifestUrl] of Object.entries(config.manifests)) {
     expect(images.length).toBe(7)
 
     for (const image of images) {
-      test(`${image.name} image`, async () => {
-        expect(image.archiveFileName, 'archive to be a gzip').toContain('.gz')
-        expect(image.archiveUrl, 'archive url to be a gzip').toContain('.gz')
+      describe(`${image.name} image`, async () => {
+        test('gzip archive', () => {
+          expect(image.archiveFileName, 'archive to be a gzip').toContain('.gz')
+          expect(image.archiveUrl, 'archive url to be a gzip').toContain('.gz')
+        })
 
         if (image.name === 'system') {
-          expect(image.sparse, 'system image to be sparse').toBe(true)
-          expect(image.fileName, 'system image to be skip chunks').toContain('-skip-chunks.img')
-          expect(image.archiveUrl, 'system image to point to skip chunks').toContain('-skip-chunks.img.gz')
+          test('alt image', () => {
+            expect(image.sparse, 'system image to be sparse').toBe(true)
+            expect(image.fileName, 'system image to be skip chunks').toContain('-skip-chunks.img')
+            expect(image.archiveUrl, 'system image to point to skip chunks').toContain('-skip-chunks.img.gz')
+          })
         }
 
-        // TODO: download and calculate checksum?
-        const response = await fetch(image.archiveUrl, { method: 'HEAD' })
-        expect(response.ok, 'to be uploaded').toBe(true)
+        test('image and checksum', async () => {
+          const response = await fetch(image.archiveUrl)
+          expect(response.ok, 'to be uploaded').toBe(true)
+
+          const inflator = new pako.Inflate()
+          const shaObj = new jsSHA('SHA-256', 'UINT8ARRAY')
+
+          inflator.onData = function (chunk) {
+            shaObj.update(chunk)
+          }
+
+          inflator.onEnd = function (status) {
+            expect(status, 'to decompress').toBe(0)
+
+            const checksum = shaObj.getHash('HEX')
+            expect(checksum, 'to match').toBe(image.checksum)
+          }
+
+          const reader = response.body.getReader()
+          while (true) {
+            const { done, value } = await reader.read()
+            if (done) break
+            inflator.push(value)
+          }
+        }, 15 * 60 * 1000)
       })
     }
   })
