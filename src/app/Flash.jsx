c16d2eb6c6b899aa37d1631307395052ce98a715
bongbui321 <hoang.bui.vh@gmail.com>
2024-03-21 00:23:59 -0400

    update instruction

--- src/app/Flash.jsx
+++ src/app/Flash.jsx
@@ -248,7 +248,7 @@ export default function Flash() {
       {title === "Lost connection" && (
         <>
           <span className={`text-xl dark:text-white px-8 max-w-xl`}>
-            If you are on Linux, make sure to run the script below in your terminal before connecting
+            If you are on Linux, make sure to run the script below in your terminal after plugging in your device.
           </span>
           <pre className="bg-gray-200 dark:bg-gray-800 rounded-md p-4 overflow-x-auto">
             <code className="font-mono text-base text-gray-800 dark:text-gray-200 bg-gray-300 dark:bg-gray-700 rounded-md p-2">
