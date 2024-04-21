6bba97b2cbbdbf39e6cdb34f032b63011d37b06c
bongbui321 <hoang.bui.vh@gmail.com>
2024-04-21 00:32:41 -0400

    new manifest test

--- src/utils/manifest.test.js
+++ src/utils/manifest.test.js
@@ -1,44 +1,10 @@
 import { expect, test, vi } from 'vitest'
 
-import * as Comlink from 'comlink'
-
 import config from '../config'
 import { getManifest } from './manifest'
 
-async function getImageWorker() {
-  let imageWorker
-
-  vi.mock('comlink')
-  vi.mocked(Comlink.expose).mockImplementation(worker => {
-    imageWorker = worker
-    imageWorker.init()
-  })
-
-  await import('./../workers/image.worker')
-
-  return imageWorker
-}
-
 for (const [branch, manifestUrl] of Object.entries(config.manifests)) {
   describe(`${branch} manifest`, async () => {
-    const imageWorkerFileHandler = {
-      getFile: vi.fn(),
-      createWritable: vi.fn().mockImplementation(() => ({
-        write: vi.fn(),
-        close: vi.fn(),
-      })),
-    }
-
-    global.navigator = {
-      storage: {
-        getDirectory: () => ({
-          getFileHandle: () => imageWorkerFileHandler,
-        })
-      }
-    }
-
-    const imageWorker = await getImageWorker()
-
     const images = await getManifest(manifestUrl)
 
     // Check all images are present
@@ -47,7 +13,6 @@ for (const [branch, manifestUrl] of Object.entries(config.manifests)) {
     for (const image of images) {
       describe(`${image.name} image`, async () => {
         test('xz archive', () => {
-          expect(image.archiveFileName, 'archive to be in xz format').toContain('.xz')
           expect(image.archiveUrl, 'archive url to be in xz format').toContain('.xz')
         })
 
@@ -58,17 +23,6 @@ for (const [branch, manifestUrl] of Object.entries(config.manifests)) {
             expect(image.archiveUrl, 'system image to point to skip chunks').toContain('-skip-chunks-')
           })
         }
-
-        test('image and checksum', async () => {
-          imageWorkerFileHandler.getFile.mockImplementation(async () => {
-            const response = await fetch(image.archiveUrl)
-            expect(response.ok, 'to be uploaded').toBe(true)
-
-            return response.blob()
-          })
-      
-          await imageWorker.unpackImage(image)
-        }, 8 * 60 * 1000)
       })
     }
   })
