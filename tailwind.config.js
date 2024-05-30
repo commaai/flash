f1fe9ebe85a18d00eaf82cfa4ce84e809a9bc269
wgenh <wgenh@users.noreply.github.com>
2024-05-30 23:14:25 +0300

    Switch to svelte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+export default {
+    content: [
+        './src/**/*.{html,js,svelte,ts}',
+    ],
+    theme: {
+      extend: {
+        backgroundImage: {
+          'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+          'gradient-conic':
+            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        },
       },
     },
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
 }
