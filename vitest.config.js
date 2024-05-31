567a36e63c660c27429de3924a288e24e59634b2
wgenh <wgenh@users.noreply.github.com>
2024-05-31 19:23:45 +0300

    Fix tests

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vitest/config'
-import path from 'path'
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+import { sveltekit } from '@sveltejs/kit/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [sveltekit()],
   resolve: {
     alias: {
       '$lib': path.resolve(__dirname, './src/lib'),
