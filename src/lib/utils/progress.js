79059f589bc9d477eaeefb4c423f54981742a5a0
wgenh <wgenh@users.noreply.github.com>
2024-05-31 22:38:05 +0300

    quick fix

--- src/lib/utils/progress.js
+++ src/lib/utils/progress.js
@@ -5,6 +5,7 @@
  * @param {number} onProgress
  * @returns {(number)[]}
  */
+// eslint-disable-next-line no-unused-vars
 export function createSteps(steps, onProgress) {
   const stepWeights = typeof steps === 'number' ? Array(steps).fill(1) : steps
 
