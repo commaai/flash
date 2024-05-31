83aeb450afbf70270e3d61fcf8b6f53081cc2f7f
wgenh <wgenh@users.noreply.github.com>
2024-05-31 18:59:59 +0300

    Fix path

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '$lib': path.resolve(__dirname, './src/lib'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
