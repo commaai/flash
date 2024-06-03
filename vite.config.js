b5e3aa92aa593035f8b52546c956694e899af33c
wgenh <wgenh@users.noreply.github.com>
2024-06-03 21:52:28 +0300

    delete vitest.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
+import { defineConfig } from 'vite';
+import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import {svelteTesting} from '@testing-library/svelte/vite';
+
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), svelteTesting()],
+	resolve: {
+	  alias: {
+		'$lib': path.resolve(__dirname, './src/lib'),
+	  },
+	},
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-});
+	  globals: true,
+	  environment: 'jsdom',
+	  setupFiles: ['@vitest/web-worker', './src/test/setup.js']
+	},
+  })
+  
