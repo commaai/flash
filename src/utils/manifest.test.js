8381ab2b80adf946a82d29dbb94ca171f434f870
Andrei Radulescu <andi.radulescu@gmail.com>
2024-09-09 20:37:06 +0300

    remove system-skip-chunks image

--- src/utils/manifest.test.js
+++ src/utils/manifest.test.js
@@ -52,10 +52,8 @@ for (const [branch, manifestUrl] of Object.entries(config.manifests)) {
         })
 
         if (image.name === 'system') {
-          test('alt image', () => {
-            expect(image.sparse, 'system image to be sparse').toBe(true)
-            expect(image.fileName, 'system image to be skip chunks').toContain('-skip-chunks-')
-            expect(image.archiveUrl, 'system image to point to skip chunks').toContain('-skip-chunks-')
+          test('system image', () => {
+            expect(image.sparse, 'system image to not be sparse').toBe(false)
           })
         }
 
