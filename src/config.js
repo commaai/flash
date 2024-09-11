e8a76e99bfff9f99be283b8a9b423c1eeec9b2df
Andrei Radulescu <andi.radulescu@gmail.com>
2024-09-11 15:17:07 +0300

    switch back to release manifest (#57)
    * switch back to release

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   manifests: {
-    // release3: 'https://raw.githubusercontent.com/commaai/openpilot/release3/system/hardware/tici/agnos.json',
-    master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/agnos.json',
+    release: 'https://raw.githubusercontent.com/commaai/openpilot/release3/system/hardware/tici/agnos.json',
+    // master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/agnos.json',
   },
 }
 
