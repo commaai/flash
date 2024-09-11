d6c822b16a1939741d64de1e5a2bcade4a0c33b5
Andrei Radulescu <andi.radulescu@gmail.com>
2024-09-11 23:27:53 +0300

    Fix manifest tests (#58)
    * both manifests
    
    * fixed manifest.test
    
    * timeout-minutes: 15

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   manifests: {
     release: 'https://raw.githubusercontent.com/commaai/openpilot/release3/system/hardware/tici/agnos.json',
-    // master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/agnos.json',
+    master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/agnos.json',
   },
 }
 
