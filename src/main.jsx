14c8957ef8078b8f23666c44e7762914a9a11a63
Unies Ananda Raja <uanandaraja@gmail.com>
2024-11-23 22:10:16 +0700

    perf: load plausible async

--- src/main.jsx
+++ src/main.jsx
@@ -5,3 +5,9 @@ import "./index.css";
 import App from "./app";
 
 render(() => <App />, document.getElementById("root"));
+
+const script = document.createElement("script");
+script.defer = true;
+script.dataset.domain = "flash.comma.ai";
+script.src = "https://plausible.io/js/script.outbound-links.js";
+document.body.appendChild(script);
