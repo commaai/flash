1f889898fa8fd72dfb4a2a59bdb0daf358e049e5
Unies Ananda Raja <uanandaraja@gmail.com>
2024-11-23 20:56:10 +0700

    refactor: replace react with solid

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,21 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [solid()],
   resolve: {
     alias: [
-      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
     ],
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
   },
-})
+});
