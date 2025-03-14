9d87a4b114ac729f86fea18268586ea870473566
Cameron Clough <cameronjclough@gmail.com>
2025-03-14 14:37:13 +0000

    test both

--- src/utils/manifest.test.js
+++ src/utils/manifest.test.js
@@ -67,7 +67,7 @@ for (const [branch, manifestUrl] of Object.entries(config.manifests)) {
 
         test.skipIf(image.name === 'system' && !MANIFEST_BRANCH)('download', async () => {
           await imageWorker.downloadImage(image)
-        }, { timeout: (image.name === 'system' ? 11 * 60 : 24) * 1000 })
+        }, { timeout: (image.name === 'system' ? 9 * 60 : 20) * 1000 })
       })
     }
   })
