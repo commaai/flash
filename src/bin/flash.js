0615ab7871d17f5be6eefda40c802f7d87fe9ce2
Cameron Clough <cameronjclough@gmail.com>
2025-03-19 17:59:02 +0000

    use in script

--- src/bin/flash.js
+++ src/bin/flash.js
@@ -3,6 +3,8 @@ import { readableStreamToBlob } from 'bun'
 import { createProgress, createQdl } from '@commaai/qdl/cli'
 import { XzReadableStream } from 'xz-decompress'
 
+import { checkCompatibleDevice } from '../utils/qdl'
+
 /**
  * @typedef {Object} Partition
  * @property {number} lun
@@ -52,40 +54,12 @@ const qdl = await createQdl()
 const storageInfo = await qdl.getStorageInfo()
 
 console.debug('UFS Serial:', storageInfo.serial_num.toString(16).padStart(8, '0'))
-
-// Should be the same for all comma 3/3X
-assert(storageInfo.block_size === 4096)
-assert(storageInfo.page_size === 4096)
-assert(storageInfo.num_physical === 6)
-assert(storageInfo.mem_type === 'UFS')
-
-let userdataImage = null;
-
-// comma three
-// userdata	start 6159400 size 7986131
-if (storageInfo.prod_name === 'H28S7Q302BMR' && storageInfo.manufacturer_id === 429 &&
-    storageInfo.fw_version === '205' && storageInfo.total_blocks === 14145536) {
-  userdataImage = 'userdata_30'
-}
-
-// comma 3X
-// userdata	start 6159400 size 23446483
-if (storageInfo.prod_name === 'SDINDDH4-128G   1308' && storageInfo.manufacturer_id === 325 &&
-    storageInfo.fw_version === '308' && storageInfo.total_blocks === 29605888) {
-  userdataImage = 'userdata_89'
-}
-// unknown userdata sectors
-if (storageInfo.prod_name === 'SDINDDH4-128G   1272' && storageInfo.manufacturer_id === 325 &&
-    storageInfo.fw_version === '272' && storageInfo.total_blocks === 29775872) {
-  userdataImage = 'userdata_90'
-}
-
+const userdataImage = checkCompatibleDevice(storageInfo)
 if (!userdataImage) {
   console.error('Could not identify device by UFS chip')
   console.debug(storageInfo)
   process.exit(1)
 }
-
 console.debug('Detected userdata image:', userdataImage)
 
 const manifestUrl = 'https://raw.githubusercontent.com/commaai/openpilot/release3-staging/system/hardware/tici/all-partitions.json'
