55fb32f8c5357179d244897b1424d2b43b070a84
Cameron Clough <cameronjclough@gmail.com>
2025-03-19 19:30:47 +0000

    use new manifest

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 const config = {
   manifests: {
-    //release: 'https://raw.githubusercontent.com/commaai/openpilot/release3/system/hardware/tici/agnos.json',
-    release: 'https://raw.githubusercontent.com/commaai/openpilot/release3-staging/system/hardware/tici/agnos.json',
-    master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/agnos.json',
+    release: 'https://raw.githubusercontent.com/commaai/openpilot/release3/system/hardware/tici/all-partitions.json',
+    master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/all-partitions.json',
   },
   loader: {
     url: 'https://raw.githubusercontent.com/commaai/flash/master/src/QDL/programmer.bin',
--- src/utils/manifest.js
+++ src/utils/manifest.js
@@ -5,16 +5,16 @@
  */
 export class ManifestImage {
   /**
-   * Partition name
+   * Image name
    * @type {string}
    */
   name
-
   /**
-   * SHA-256 checksum of the unpacked image, encoded as a hex string
+   * Partition name
    * @type {string}
    */
-  checksum
+  partitionName
+
   /**
    * Size of the unpacked and unsparsified image in bytes
    * @type {number}
@@ -25,13 +25,17 @@ export class ManifestImage {
    * @type {boolean}
    */
   sparse
+  /**
+   * Whether there are multiple slots for this partition
+   * @type {boolean}
+   */
+  hasAB
 
   /**
    * Name of the image file
    * @type {string}
    */
   fileName
-
   /**
    * Name of the image archive file
    * @type {string}
@@ -51,37 +55,18 @@ export class ManifestImage {
 
   constructor(json) {
     this.name = json.name
+    this.partitionName = json.name.startsWith('userdata_') ? 'userdata' : json.name
+
+    this.size = json.size
     this.sparse = json.sparse
+    this.hasAB = json.has_ab
 
     this.fileName = `${this.name}-${json.hash_raw}.img`
-    this.checksum = json.hash
     this.archiveUrl = json.url
-    this.size = json.size
-
     this.archiveFileName = this.archiveUrl.split('/').pop()
+
     this.compressed = this.archiveFileName.endsWith('.xz')
   }
-}
-
-/**
- * @param {string} text
- * @returns {ManifestImage[]}
- */
-export function createManifest(text) {
-  const expectedPartitions = ['aop', 'devcfg', 'xbl', 'xbl_config', 'abl', 'boot', 'system']
-  const partitions = JSON.parse(text).map((image) => new ManifestImage(image))
-
-  // Sort into consistent order
-  partitions.sort((a, b) => expectedPartitions.indexOf(a.name) - expectedPartitions.indexOf(b.name))
-
-  // Check that all partitions are present
-  // TODO: should we prevent flashing if there are extra partitions?
-  const missingPartitions = expectedPartitions.filter((name) => !partitions.some((image) => image.name === name))
-  if (missingPartitions.length > 0) {
-    throw new Error(`Manifest is missing partitions: ${missingPartitions.join(', ')}`)
-  }
-
-  return partitions
 }
 
 /**
@@ -91,5 +76,5 @@ export function createManifest(text) {
 export function getManifest(url) {
   return fetch(url)
     .then((response) => response.text())
-    .then(createManifest)
+    .then((text) => JSON.parse(text).map((image) => new ManifestImage(image)))
 }
