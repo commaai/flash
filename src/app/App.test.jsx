b98b9c5435a9ce15276788f4d748001fbad2cf1c
AmbiguousX <126121852+AmbiguousX@users.noreply.github.com>
2025-05-05 16:55:26 -0700

    Replace React implementation with iframe solution

--- src/app/App.test.jsx
+++ src/app/App.test.jsx
@@ -1,10 +1,10 @@
-import { Suspense } from 'react'
 import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 
 import App from '.'
 
 test('renders without crashing', () => {
-  render(<Suspense fallback="loading"><App /></Suspense>)
-  expect(screen.getByText('flash.comma.ai')).toBeInTheDocument()
+  render(<App />)
+  // Check for the iframe with the correct source
+  expect(screen.getByTitle('flash.comma.ai')).toHaveAttribute('src', 'https://flash-4fy.pages.dev/')
 })
