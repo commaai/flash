63eee2346a09fc0fec22ad177491fe1ba53a66b9
dwk601 <10829539@uvu.edu>
2025-06-20 12:10:20 -0400

    refactor: update project to use solid-js instead of react and adjust configurations

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,43 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import solid from 'vite-plugin-solid'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    solid({
+      // Enable optimizations
+      babel: {
+        compact: true,
+      },
+    }),
+  ],
+  build: {
+    // Aggressive minification
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log'],
+      },
+    },
+    // Optimize chunks
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['solid-js'],
+          qdl: ['@commaai/qdl'],
+        },
+      },
+    },
+    // Target modern browsers for smaller output
+    target: 'es2022',
+    // Remove polyfills for smaller bundle
+    cssCodeSplit: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
+    setupFiles: './src-solid/test/setup.js',
   },
 })
