b3d150716e3f79045b02bc1a94712529ccbc2815
dwk601 <10829539@uvu.edu>
2025-06-22 11:02:40 -0400

    refactor: update App.test.jsx to use Solid.js instead of React

--- src/app/App.test.jsx
+++ src/app/App.test.jsx
@@ -1,10 +1,10 @@
-import { Suspense } from 'react'
+import { Suspense } from 'solid-js'
 import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@solidjs/testing-library'
 
 import App from '.'
 
 test('renders without crashing', () => {
-  render(<Suspense fallback="loading"><App /></Suspense>)
+  render(() => <Suspense fallback="loading"><App /></Suspense>)
   expect(screen.getByText('flash.comma.ai')).toBeInTheDocument()
 })
