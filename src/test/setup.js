321b771153e2a524fbd2f451f6c46f0911bc1641
Samar Sheikh <samarsheikh001@gmail.com>
2025-07-10 14:25:22 +0530

    Remove Flash UI components and update test setup

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,1 +1,27 @@
 import '@testing-library/jest-dom'
+
+// Setup for vanilla JS tests
+global.ResizeObserver = class ResizeObserver {
+  constructor(cb) {
+    this.cb = cb;
+  }
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+// Mock File System Access API for tests
+global.navigator = global.navigator || {};
+global.navigator.storage = {
+  getDirectory: () => Promise.resolve({
+    remove: () => Promise.resolve(),
+    getFileHandle: () => Promise.resolve({
+      createWritable: () => Promise.resolve({
+        write: () => Promise.resolve(),
+        close: () => Promise.resolve()
+      }),
+      getFile: () => Promise.resolve(new Blob())
+    })
+  }),
+  estimate: () => Promise.resolve({ quota: 10 * 1024 * 1024 * 1024 })
+};
