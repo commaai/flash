fb96e3ecabf33be3e7e9ec07ab70089757191efe
Maxime Desroches <desroches.maxime@gmail.com>
2025-11-21 13:53:09 -0800

    update mici manifest

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   manifests: {
-    release_mici: 'https://raw.githubusercontent.com/commaai/openpilot/86953538914a7f56f4a345d35b9f4ade6913899b/system/hardware/tici/all-partitions.json',
+    release_mici: 'https://raw.githubusercontent.com/commaai/openpilot/ccf7361798a2b7ff36f5065dffb602eb40c22302/system/hardware/tici/all-partitions.json',
     release_tizi: 'https://raw.githubusercontent.com/commaai/openpilot/927548621be1be0c2c9063868b93d1f5020904de/system/hardware/tici/all-partitions.json',
     release_tici: 'https://raw.githubusercontent.com/commaai/openpilot/927548621be1be0c2c9063868b93d1f5020904de/system/hardware/tici/all-partitions.json',
     master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/all-partitions.json',
